e195b53ed78d64fe69525a5beb982b52ddbe1cd7
Alan Roy <alanroyff101@gmail.com>
2025-10-30 18:27:46 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/MyMedia/", // 👈 THIS is the important line
   server: {
     host: "::",
     port: 8080,
@@ -12,8 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"), 
+      "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "/", 
 }));
